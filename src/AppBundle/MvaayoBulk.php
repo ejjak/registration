3f4fdda3deff11e309336f1fd9bf5cf083e04606
Ejjak <ezzak@demo.co.in>
2016-10-20 12:22:06 +0530

    sdf

--- src/AppBundle/MvaayoBulk.php
+++ src/AppBundle/MvaayoBulk.php
@@ -77,7 +77,7 @@ class MvaayoBulk extends Controller
         if (empty ($buffer)) {
             echo " buffer is empty ";
         } else {
-            echo $buffer;
+//            echo $buffer;
         }
         curl_close($ch);
         return $buffer;
